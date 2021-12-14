fa688f32fc1e53ee453aed6b1ddadc613ba28a37
John Gutierrez <djterez88@gmail.com>
2021-12-14 13:48:19 -0800

    Setup app to be seen on django server

--- chatsystem/chatting/views.py
+++ chatsystem/chatting/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    hello = "Hello World"
+    
+    context = {'hello': hello}
+    return render(request, "chatting/index.html", context)
