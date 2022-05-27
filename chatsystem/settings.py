cf808070f8ff6774fc60400345dc439b043dc060
John Gutierrez <djterez88@gmail.com>
2022-05-26 19:39:08 -0700

    Updated allowed hosts

--- chatsystem/settings.py
+++ chatsystem/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-39&k$im+n4yw+9h8y!=+jq7n(#4=e0y^=(g^!0%&anws*alsji
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'john-chat-1.herokuapp.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
