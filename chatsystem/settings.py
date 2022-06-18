c78470fecfad3010a7f0d3569cc2d17d1d3edb18
John Gutierrez <djterez88@gmail.com>
2022-06-17 21:09:04 -0700

    Added custom 404 page, but now images don't display

--- chatsystem/settings.py
+++ chatsystem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-39&k$im+n4yw+9h8y!=+jq7n(#4=e0y^=(g^!0%&anws*alsji'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'john-chat-1.herokuapp.com',
--- chatting/views.py
+++ chatting/views.py
@@ -17,6 +17,9 @@ import random
 
 # Create your views here.
 
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
+
 @login_required(login_url='login')
 def index(request):
     # post = Post.objects.all()
